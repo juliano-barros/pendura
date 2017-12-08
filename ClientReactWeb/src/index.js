b38cd00275c92710dce48007be317db7b0f04026
Juliano <julianorafael@gmail.com>
2017-12-08 11:01:11 -0300

    Adding files and up to date all dependencies from react

--- ClientReactWeb/src/index.js
+++ ClientReactWeb/src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 
 import App from './components/app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
+	<BrowserRouter>
+  		<div>
+			<Switch>
+				<Route path="/" component={App} />
+			</Switch>
+  		</div>
+  	</BrowserRouter>
+    </Provider>
   , document.querySelector('.container'));
